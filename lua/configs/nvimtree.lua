567a5e2f03d5defbea08bb8582cb1723d87e5690
mathjiajia <jiajia2u@vip.qq.com>
2022-02-22 09:46:17 +0800

    chore(nvim): update `nvim-tree` config follow upstream

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -1,8 +1,4 @@
 -- settings
-local picker_exclude = {
-	filetype = { 'notify', 'packer', 'qf' },
-	buftype = { 'terminal' },
-}
 local special_files = {
 	['README.md'] = true,
 	['LICENSE'] = true,
@@ -44,15 +40,10 @@ vim.g.nvim_tree_highlight_opened_files = 2 -- 0 by default, will enable folder a
 vim.g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
 vim.g.nvim_tree_add_trailing = 0 -- 0 by default, append a trailing slash to folder names
 vim.g.nvim_tree_group_empty = 1 --  0 by default, compact folders that only contain a single folder into one node in the file tree
-vim.g.nvim_tree_disable_window_picker = 0 -- 0 by default, will disable the window picker.
 vim.g.nvim_tree_icon_padding = ' ' -- one space by default, used for rendering the space between the icon and the filename.
 vim.g.nvim_tree_symlink_arrow = ' >> ' --  defaults to ' ➛ '. used as a separator between symlinks' source and target.
 vim.g.nvim_tree_respect_buf_cwd = 0 -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
 vim.g.nvim_tree_create_in_closed_folder = 1 -- 0 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
-vim.g.nvim_tree_window_picker_exclude = picker_exclude
--- Dictionary of buffer option names mapped to a list of option values that
--- indicates to the window picker that the buffer's window should not be
--- selectable.
 vim.g.nvim_tree_special_files = special_files
 -- List of filenames that gets highlighted with NvimTreeSpecialFile
 vim.g.nvim_tree_show_icons = show_icons
@@ -96,6 +87,7 @@ require('nvim-tree').setup {
 	open_on_setup = false,
 	ignore_ft_on_setup = {},
 	auto_close = true,
+	auto_reload_on_write = true,
 	open_on_tab = false,
 	hijack_cursor = false,
 	update_cwd = true,
@@ -154,6 +146,17 @@ require('nvim-tree').setup {
 		},
 		open_file = {
 			quit_on_open = false,
+			window_picker = {
+				enable = true,
+				chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
+				exclude = {
+					filetype = {
+						'notify',
+						'packer',
+						'qf',
+					},
+				},
+			},
 		},
 	},
 }
