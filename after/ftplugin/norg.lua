5a162e8504c3bddd5c68fb10c9eaf89cfd82818c
mathjiajia <jiajia2u@vip.qq.com>
2022-02-26 00:32:58 +0800

    chore(nvim): update with `master`

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -4,9 +4,3 @@ vim.bo.textwidth = 120
 vim.opt_local.spell = true
 vim.bo.spelllang = 'en_gb'
 vim.keymap.set('i', '<M-l>', '<C-g>u<Esc>[s1z=`]a<C-g>u', { buffer = true, desc = 'Fix Last Miss-Spelling' })
-
--- local cp = require('colors')
-
--- vim.api.nvim_set_hl(0, 'NeorgTag', { fg = cp.lavender })
--- vim.api.nvim_set_hl(0, 'NeorgTagName', { fg = cp.red })
--- vim.api.nvim_set_hl(0, 'NeorgTagNameWord', { fg = cp.lavender, underline = true })
