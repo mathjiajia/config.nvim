2d62d6401bc893793da4d4524acf9fefa23ed5ee
mathjiajia <jiajia2u@vip.qq.com>
2022-03-08 14:03:32 +0800

    feat(nvim): less lazy loading

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,12 +1,10 @@
--- Install packer
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 	vim.fn.termopen(('git clone https://github.com/wbthomason/packer.nvim %q'):format(install_path))
 end
 
--- Only required if you have packer configured as `opt`
-vim.cmd([[packadd packer.nvim]])
+-- vim.cmd([[packadd packer.nvim]])
 
 vim.api.nvim_create_augroup('packer_user_config', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
@@ -22,37 +20,34 @@ local function spec(use)
 
 	use {
 		'rcarriga/nvim-notify',
-		config = function()
+		config = [[
 			require('notify').setup {}
 			vim.notify = require('notify')
-		end,
+		]],
 	}
 
 	use {
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate',
-		requires = {},
-		config = function()
-			require('configs.treesitter')
-		end,
+		config = [[require('configs.treesitter')]],
 	}
-	use { 'nvim-treesitter/playground', cmd = { 'TSPlaygroundToggle', 'TSHighlightCapturesUnderCursor' } }
-	use { 'p00f/nvim-ts-rainbow' }
+	use {
+		{ 'nvim-treesitter/playground' },
+		{ 'p00f/nvim-ts-rainbow' },
+		{
+			'lukas-reineke/indent-blankline.nvim',
+			config = [[require('configs.indentline')]],
+		},
+	}
 
 	use {
 		'simrat39/symbols-outline.nvim',
-		keys = { { 'n', '<M-o>' } },
-		config = function()
-			require('configs.outline')
-		end,
+		config = [[require('configs.outline')]],
 	}
 
-	-- LSP Stuff
 	use {
 		'neovim/nvim-lspconfig',
-		config = function()
-			require('lsp')
-		end,
+		config = [[require('lsp')]],
 	}
 	use {
 		{ 'folke/lua-dev.nvim', module = 'lua-dev' },
@@ -61,26 +56,18 @@ local function spec(use)
 			'jose-elias-alvarez/null-ls.nvim',
 			ft = { 'fish', 'html', 'json', 'lua', 'markdown', 'yaml' },
 			requires = { 'nvim-lua/plenary.nvim' },
-			config = function()
-				require('lsp.null-ls')
-			end,
+			config = [[require('lsp.null-ls')]],
 		},
 		{
 			'j-hui/fidget.nvim',
-			after = 'nvim-lspconfig',
-			config = function()
-				require('fidget').setup { text = { spinner = 'line' } }
-			end,
+			config = [[require('fidget').setup { text = { spinner = 'line' } }]],
 		},
 	}
 
-	-- Autocompletion
 	use {
 		'hrsh7th/nvim-cmp',
 		event = 'InsertEnter',
-		config = function()
-			require('configs.cmp')
-		end,
+		config = [[require('configs.cmp')]],
 	}
 	use {
 		{ 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
@@ -93,163 +80,88 @@ local function spec(use)
 	use {
 		'L3MON4D3/LuaSnip',
 		module = 'luasnip',
-		config = function()
-			require('configs.luasnip')
-		end,
+		config = [[require('configs.luasnip')]],
+	}
+	use {
+		'ZhiyuanLck/smart-pairs',
+		event = 'InsertEnter',
+		config = [[require('configs.pairs')]],
 	}
 	use {
 		'github/copilot.vim',
 		cmd = 'Copilot',
-		config = function()
-			require('configs.copilot')
-		end,
+		config = [[require('configs.copilot')]],
 	}
 
-	-- Language Stuff
 	use {
 		'lervag/vimtex',
 		ft = 'tex',
-		config = function()
-			require('configs.vimtex')
-		end,
+		config = [[require('configs.vimtex')]],
 	}
 	use {
 		{
 			'nvim-neorg/neorg',
 			ft = 'norg',
-			config = function()
-				require('configs.neorg')
-			end,
+			config = [[require('configs.neorg')]],
 		},
 		{ 'nvim-neorg/neorg-telescope', opt = true },
 	}
 
-	-- Telescope nonsense
 	use {
 		'nvim-telescope/telescope.nvim',
-		cmd = 'Telescope',
-		module = 'telescope',
-		keys = {
-			{ 'n', '<leader>f' },
-			{ 'n', '<leader><space>' },
-			{ 'n', '<leader><leader>' },
-		},
 		requires = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			require('configs.telescope')
-		end,
+		config = [[require('configs.telescope')]],
 	}
 	use {
-		{ 'nvim-telescope/telescope-file-browser.nvim', opt = true },
-		{
-			'nvim-telescope/telescope-frecency.nvim',
-			opt = true,
-			requires = { 'tami5/sqlite.lua', opt = true },
-		},
-		{ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', opt = true },
-		{ 'nvim-telescope/telescope-project.nvim', opt = true },
+		{ 'nvim-telescope/telescope-file-browser.nvim' },
+		{ 'nvim-telescope/telescope-frecency.nvim', requires = 'tami5/sqlite.lua' },
+		{ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+		{ 'nvim-telescope/telescope-project.nvim' },
 	}
 
-	-- File Explorer
 	use {
 		'kyazdani42/nvim-tree.lua',
-		keys = { { 'n', '<M-t>' } },
+		key = { { 'n', '<M-t>' } },
 		requires = { 'kyazdani42/nvim-web-devicons' },
-		config = function()
-			require('configs.nvimtree')
-		end,
+		config = [[require('configs.nvimtree')]],
 	}
 
-	-- Editors
-	use { -- Git Integration
+	use {
 		'lewis6991/gitsigns.nvim',
 		requires = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			require('configs.gitsigns')
-		end,
+		config = [[require('configs.gitsigns')]],
 	}
-	use { -- Comment/Uncomment
+	use {
 		'numToStr/Comment.nvim',
-		keys = {
-			{ 'n', 'gb' },
-			{ 'n', 'gc' },
-			{ 'v', 'gb' },
-			{ 'v', 'gc' },
-		},
-		config = function()
-			require('Comment').setup()
-		end,
-	}
-	use { -- indentline
-		'lukas-reineke/indent-blankline.nvim',
-		config = function()
-			require('configs.indentline')
-		end,
-	}
-	use { -- Delimiters
-		'ZhiyuanLck/smart-pairs',
-		event = 'InsertEnter',
-		config = function()
-			require('configs.pairs')
-		end,
-	}
-	use { -- Surrounding
-		'ur4ltz/surround.nvim',
-		keys = {
-			{ 'n', 'sa' },
-			{ 'n', 'sr' },
-			{ 'n', 'sd' },
-			{ 'x', 's' },
-		},
-		config = function()
-			require('configs.surround')
-		end,
-	}
-	use { -- Motions
-		'phaazon/hop.nvim',
-		keys = {
-			{ 'n', '<leader>h' },
-			{ 'x', '<leader>h' },
-			{ 'o', '<leader>h' },
-		},
-		config = function()
-			require('configs.hop')
-		end,
-	}
-	use { -- Search Panel
-		'windwp/nvim-spectre',
-		keys = { { 'n', '<leader>r' } },
-		config = function()
-			require('configs.spectre')
-		end,
-	}
-	use { -- Sessions Management
-		'Shatur/neovim-session-manager',
-		keys = { { 'n', '<leader>s' } },
-		config = function()
-			require('configs.session')
-		end,
+		config = [[require('Comment').setup()]],
 	}
 
-	-- Terminal Emulator
-	use { --floating terminal
-		'numtostr/FTerm.nvim',
-		module = 'FTerm',
-		keys = {
-			{ 'n', '<M-i>' },
-			{ 'n', '<M-g>' },
-		},
-		config = function()
-			require('configs.fterm')
-		end,
+	use {
+		'ur4ltz/surround.nvim',
+		config = [[require('configs.surround')]],
 	}
-	-- use {
-	-- 	'dstein64/vim-startuptime',
-	-- 	cmd = 'StartupTime',
-	-- 	config = function()
-	-- 		vim.g.startuptime_tries = 5
-	-- 	end,
-	-- }
+	use {
+		'phaazon/hop.nvim',
+		config = [[require('configs.hop')]],
+	}
+	use {
+		'windwp/nvim-spectre',
+		config = [[require('configs.spectre')]],
+	}
+	use {
+		'Shatur/neovim-session-manager',
+		config = [[require('configs.session')]],
+	}
+
+	use {
+		'numtostr/FTerm.nvim',
+		config = [[require('configs.fterm')]],
+	}
+	use {
+		'dstein64/vim-startuptime',
+		cmd = 'StartupTime',
+		config = [[vim.g.startuptime_tries = 20]],
+	}
 end
 
 require('packer').startup {
