87906dbe50463ecddc12c82609d280abaf1ed840
mathjiajia <jiajia2u@vip.qq.com>
2022-04-02 21:21:30 +0800

    feat(builtin): disable more builtin plugins

--- init.lua
+++ init.lua
@@ -18,11 +18,19 @@ end
 vim.g.mapleader = ','
 
 -- TURN OFF SOME BUILTIN PLUGINS
-vim.g.loaded_gzip = 1
-vim.g.loaded_matchparen = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_tarPlugin = 1
 vim.g.loaded_2html_plugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_zip = 1
 vim.g.loaded_zipPlugin = 1
 
 vim.g.do_filetype_lua = 1
@@ -30,3 +38,4 @@ vim.g.did_load_filetypes = 0
 
 -- NEOVIM BUILTIN LSP CONFIGURATION
 require('lsp')
+-- require('plugins')
