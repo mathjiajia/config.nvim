6f38751f1fbc9c5d78d72a6d95e5f4240fcddbf5
mathjiajia <jiajia2u@vip.qq.com>
2022-04-02 22:27:24 +0800

    chore(query): add a revisement

--- after/queries/latex/highlights.scm
+++ after/queries/latex/highlights.scm
@@ -1,1 +1,10 @@
-; "item"
+;; General environments
+; (begin
+;  command: _ @text.environment
+;  name: (curly_group_text
+;          (text) @text.environment.name))
+
+; (end
+;  command: _ @text.environment
+;  name: (curly_group_text
+;          (text) @text.environment.name))
