4ebb55f88d9706b70fe82d99c96219834fc9aa34
mathjiajia <jiajia2u@vip.qq.com>
2022-04-13 23:34:01 +0800

    chore(cmp): update config upstream changes

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -29,13 +29,12 @@ local cmp_kinds = {
 local cmp = require 'cmp'
 
 cmp.setup {
-	mapping = {
-		['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-		['<C-y>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true },
-		['<C-e>'] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
-	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-d>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-y>'] = cmp.mapping.confirm { select = true },
+	}),
 	snippet = {
 		expand = function(args)
 			require('luasnip').lsp_expand(args.body)
@@ -67,17 +66,22 @@ cmp.setup {
 		{ name = 'buffer', keyword_length = 4 },
 		{ name = 'rg', keyword_length = 5 },
 	},
-	-- window = {
-	-- 	completion = {
-	-- 		border = { '┌', '─', '┐', '│', '┘', '─', '└', '│' },
-	-- 		-- border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-	-- 		winhighlight = {
-	-- 			bordered = 'Normal:CmpBorderedWindow_Normal,FloatBorder:CmpBorderedWindow_FloatBorder,CursorLine:CmpBorderedWindow_CursorLine,Search:None',
-	-- 			default = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
-	-- 		},
-	-- 	},
-	-- },
+	window = {
+		completion = {
+			border = { '┌', '─', '┐', '│', '┘', '─', '└', '│' },
+			-- border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+		},
+	},
 }
 
-cmp.setup.cmdline('/', { sources = { { name = 'buffer' } } })
-cmp.setup.cmdline(':', { sources = { { name = 'path' }, { name = 'cmdline' } } })
+cmp.setup.cmdline('/', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = { { name = 'buffer' } }
+})
+
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'path' },
+		{ name = 'cmdline' } }
+})
