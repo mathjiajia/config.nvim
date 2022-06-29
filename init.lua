d8a4942def823ef5c2e3c323a37fc10fb7dc49f1
mathjiajia <jiajia2u@vip.qq.com>
2022-06-29 23:33:34 +0800

    chore(init): adjust order

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@ require 'impatient'
 
 vim.g.mapleader = ','
 
+vim.g.do_filetype_lua = 1
+vim.g.did_load_filetypes = 0
+
 -- TURN OFF SOME BUILTIN PLUGINS
 vim.g.loaded_2html_plugin = 1
 vim.g.loaded_gzip = 1
@@ -17,9 +20,6 @@ vim.g.loaded_vimball = 1
 vim.g.loaded_vimballPlugin = 1
 vim.g.loaded_zip = 1
 vim.g.loaded_zipPlugin = 1
-
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
 
 -- DISABLE REMOTE PLUGINS
 vim.g.loaded_python3_provider = 0
