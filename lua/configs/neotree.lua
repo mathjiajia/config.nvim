6a0abeb1cb6d788f9c2e184d82239b48bcf808bb
mathjiajia <jiajia2u@vip.qq.com>
2022-07-24 09:29:28 +0800

    feat(neotree): use `source_selector`

--- lua/configs/neotree.lua
+++ lua/configs/neotree.lua
@@ -1,14 +1,12 @@
 vim.g.neo_tree_remove_legacy_commands = 1
 
 require('neo-tree').setup {
-	default_component_configs = {
-		indent = { padding = 0, with_markers = true },
-	},
 	window = { width = 30 },
 	filesystem = {
 		follow_current_file = true,
 		hijack_netrw_behavior = 'open_current',
-	}
+	},
+	source_selector = { winbar = true }
 }
 
 vim.keymap.set('n', '<M-t>',
