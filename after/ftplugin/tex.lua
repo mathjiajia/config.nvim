a23e68721cd79f175b613b1328a4773c1741d922
mathjiajia <jiajia2u@vip.qq.com>
2022-07-26 09:45:59 +0800

    style(tex): formatting

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -3,5 +3,5 @@ vim.wo.conceallevel = 2
 vim.opt_local.spell = true
 vim.bo.spelllang = 'en_gb'
 vim.keymap.set('i', '<M-l>',
-    '<C-g>u<Esc>[s1z=`]a<C-g>u',
-    { buffer = true, desc = 'Fix Last Miss-Spelling' })
+	'<C-g>u<Esc>[s1z=`]a<C-g>u',
+	{ buffer = true, desc = 'Fix Last Miss-Spelling' })
