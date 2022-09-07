468a1d0d5a0d20e1ccf36e5c679c4854f6a5144a
mathjiajia <jiajia2u@vip.qq.com>
2022-09-07 21:16:59 +0800

    chore(options): separate some options

--- plugin/options.lua
+++ plugin/options.lua
@@ -1,7 +1,6 @@
 vim.o.breakindent = true
 vim.o.clipboard = 'unnamedplus'
 vim.o.cmdheight = 0
-vim.o.completeopt = 'menu,menuone,noselect'
 vim.wo.cursorline = true
 vim.o.ignorecase = true
 vim.o.laststatus = 3
@@ -10,21 +9,26 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 vim.o.pumheight = 10
 vim.o.scrolloff = 8
-vim.o.shiftwidth = 4
 vim.o.showbreak = '> '
 vim.wo.signcolumn = 'yes'
 vim.o.smartcase = true
 vim.o.smartindent = true
-vim.o.softtabstop = 4
 vim.o.splitbelow = true
 vim.o.splitright = true
 vim.bo.swapfile = false
-vim.o.tabstop = 4
 vim.o.termguicolors = true
 vim.o.undofile = true
 vim.o.updatetime = 200
 vim.o.whichwrap = 'b,s,[,]'
 vim.o.writebackup = false
+
+-- Completion
+vim.o.completeopt = 'menu,menuone,noselect'
+
+-- Tabs
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.tabstop = 4
 
 -- vim.o.background = 'light'
 vim.cmd.colorscheme 'vscode'
