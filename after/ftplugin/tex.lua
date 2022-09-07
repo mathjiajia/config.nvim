1e995dbbd82295fc2c53102176d2fc9174b0826b
mathjiajia <jiajia2u@vip.qq.com>
2022-09-07 09:04:50 +0800

    chore(tex): nonsense

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -5,3 +5,7 @@ vim.bo.spelllang = 'en_gb'
 vim.keymap.set('i', '<M-l>',
 	'<C-g>u<Esc>[s1z=`]a<C-g>u',
 	{ buffer = true, desc = 'Fix Last Miss-Spelling' })
+
+-- vim.keymap.set('n', '<M-b>',
+-- 	'<Cmd>TexlabBuild<CR>',
+-- 	{ buffer = true, desc = 'Build LaTeX' })
