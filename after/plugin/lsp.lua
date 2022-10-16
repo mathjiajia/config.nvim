84ad85662f7b4f8775d6dbc8e39e5328478656a2
mathjiajia <jiajia2u@vip.qq.com>
2022-10-17 07:59:35 +0800

    fix(lsp): updating capabilities

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -82,16 +82,14 @@ local on_attach = function(client, bufnr)
 	require('aerial').on_attach(client, bufnr)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local nvim_lsp = require 'lspconfig'
 
 require('mason-lspconfig').setup {
 	ensure_installed = { 'pyright', 'sumneko_lua', 'texlab' }
 }
 
-require('lua-dev').setup()
+require('neodev').setup({})
 
 require('mason-lspconfig').setup_handlers {
 	function(server_name)
