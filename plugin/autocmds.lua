223b7249a5c5c198fc5e638e2039f15e8f23fbdd
mathjiajia <jiajia2u@vip.qq.com>
2022-10-22 10:16:03 +0800

    feat(autcms): add `lsp` attach
    need upstream to fix

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -13,3 +13,16 @@ vim.api.nvim_create_autocmd('BufEnter', {
 	group   = 'init_nvim',
 	desc    = 'change the working directory',
 })
+
+-- vim.api.nvim_create_autocmd('LSPAttach', {
+-- 	callback = function(args)
+-- 		local bufnr = args.buf
+-- 		local client = args.data.client
+-- 		vim.notify(
+-- 			string.format('%s(%d) attached to buffer(%d)', client.name, client.id, bufnr),
+-- 			vim.log.levels.INFO,
+-- 			{ title = 'LSP' }
+-- 		)
+-- 	end,
+-- 	group    = 'init_nvim',
+-- })
