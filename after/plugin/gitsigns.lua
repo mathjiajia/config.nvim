de5903b6c83ba11b3f031d72c8cfde568f491e1b
mathjiajia <jiajia2u@vip.qq.com>
2022-10-25 08:37:03 +0800

    feat(gitsigns): attach keymaps

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,1 +1,27 @@
-require('gitsigns').setup()
+require('gitsigns').setup({
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+
+		vim.keymap.set(
+			'n',
+			']c',
+			function()
+				if vim.wo.diff then return ']c' end
+				vim.schedule(function() gs.next_hunk() end)
+				return '<Ignore>'
+			end,
+			{ expr = true, buffer = bufnr }
+		)
+
+		vim.keymap.set(
+			'n',
+			'[c',
+			function()
+				if vim.wo.diff then return '[c' end
+				vim.schedule(function() gs.prev_hunk() end)
+				return '<Ignore>'
+			end,
+			{ expr = true, buffer = bufnr }
+		)
+	end
+})
