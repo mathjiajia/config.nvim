eacd06e9e91e0f952b42a2bbc5526aed29f50c83
mathjiajia <jiajia2u@vip.qq.com>
2022-10-28 21:35:33 +0800

    refactor(snips): `context` simplify

--- lua/snips/context.lua
+++ lua/snips/context.lua
@@ -3,7 +3,7 @@ local M = {}
 local cond_obj = require('luasnip.extras.conditions')
 
 ---Check if cursor is in treesitter capture
----@param capture (string|string[])
+---@param capture string
 ---@return boolean
 local in_ts_capture = function(capture)
 	local buf = vim.api.nvim_get_current_buf()
@@ -11,21 +11,11 @@ local in_ts_capture = function(capture)
 	row = row - 1
 	col = col - 1
 
-	local get_captures_at_pos = vim.treesitter.get_captures_at_pos
+	local captures = vim.treesitter.get_captures_at_pos(buf, row, col)
 
-	local captures_at_cursor = vim.tbl_map(function(x)
-		return x.capture
-	end, get_captures_at_pos(buf, row, col))
-
-	if vim.tbl_isempty(captures_at_cursor) then
-		return false
-	elseif type(capture) == 'string' and vim.tbl_contains(captures_at_cursor, capture) then
-		return true
-	elseif type(capture) == 'table' then
-		for _, v in ipairs(capture) do
-			if vim.tbl_contains(captures_at_cursor, v) then
-				return true
-			end
+	for _, c in ipairs(captures) do
+		if c.capture == capture then
+			return true
 		end
 	end
 
@@ -35,21 +25,29 @@ end
 ---Check if cursor is in treesitter capture of 'comment'
 ---@return boolean
 local function in_comments()
-	return in_ts_capture 'comment'
+	return in_ts_capture('comment')
 end
 
 ---Check if cursor is in treesitter capture of 'text.math'
 ---@return boolean
 local function in_mathzone()
-	return in_ts_capture 'text.math'
+	return in_ts_capture('text.math')
 end
+
+local non_text = { 'text.math', 'function', 'include', 'parameter', '_name' }
 
 ---Check if cursor is in treesitter node of 'text'
 ---@return boolean
 local function in_text()
-	-- TODO: maybe use 'spell' when they fix it
+	-- TODO: use 'spell' in the furture
 	-- return in_ts_capture 'spell'
-	return not in_ts_capture({ 'text.math', 'function', 'include', '_name' })
+	for _, v in ipairs(non_text) do
+		if in_ts_capture(v) then
+			return false
+		end
+	end
+
+	return true
 end
 
 M.in_comments = cond_obj.make_condition(in_comments)
