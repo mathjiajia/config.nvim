db19fcdb47510f5408f8347fc8cf24f621cb86ee
mathjiajia <jiajia2u@vip.qq.com>
2022-10-28 16:01:06 +0800

    refactor(configs): move files

---
+++ after/plugin/neorg.lua
@@ -0,0 +1,20 @@
+-- require('neorg').setup {
+--     load = {
+--         ['core.defaults'] = {},
+--         ['core.keybinds'] = {},
+--         ['core.norg.concealer'] = {},
+--         ['core.norg.dirman'] = {
+--             config = {
+--                 workspaces = {
+--                     gtd  = '~/Documents/neorg/gtd/',
+--                     work = '~/Documents/neorg/work/',
+--                 },
+--             },
+--         },
+--         ['core.gtd.base'] = { config = { workspace = 'gtd' } },
+--         ['core.gtd.ui'] = {},
+--         ['core.norg.completion'] = { config = { engine = 'nvim-cmp' } },
+--         ['core.norg.qol.toc'] = {},
+--         ['core.integrations.telescope'] = {},
+--     },
+-- }
--- lua/configs/copilot.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set('i', '<C-j>',
-    'copilot#Accept("\\<CR>")',
-    { silent = true, script = true, expr = true, desc = 'Copilot Accept' }
-)
-
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_assume_mapped = true
