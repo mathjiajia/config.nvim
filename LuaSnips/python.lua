260c365b4dd605ad8e7e1f03440d8083b5d37079
mathjiajia <jiajia2u@vip.qq.com>
2022-11-03 19:59:54 +0800

    feat(LuaSnips): add snippet for `python`

---
+++ LuaSnips/python.lua
@@ -0,0 +1,17 @@
+local snips = {}
+
+local conds_expand = require('luasnip.extras.conditions.expand')
+local position = require('snips.position')
+
+snips = {
+	s(
+		{ trig = 'env', name = 'python3 environment', dscr = 'Declare py3 environment' },
+		{ t({ '#!/usr/bin/env python3', '' }) },
+		{
+			condition = position.on_top * conds_expand.line_begin,
+			show_condition = position.on_top * position.line_begin
+		}
+	),
+}
+
+return snips
