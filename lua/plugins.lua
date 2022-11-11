40d8ae93abe895b3b1a142ecb1247269d44f025d
mathjiajia <jiajia2u@vip.qq.com>
2022-11-11 21:42:32 +0800

    refactor(plugins): use `lewis6991/packer`

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,86 +5,86 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 		'clone',
 		'--depth',
 		'1',
-		'https://github.com/wbthomason/packer.nvim',
+		'https://github.com/lewis6991/packer.nvim',
 		install_path
 	})
 end
 
 vim.api.nvim_create_augroup('packer_user_config', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
-	command = 'source | PackerCompile',
+	command = 'source',
 	pattern = 'plugins.lua',
 	group   = 'packer_user_config',
-	desc    = 'Compile whenever plugins.lua is updated',
+	desc    = 'reload whenever plugins.lua is updated',
 })
 
-require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'lewis6991/impatient.nvim'
+local plugins = {
+	'lewis6991/packer.nvim',
+	'lewis6991/impatient.nvim',
 
-	use 'monkoose/matchparen.nvim'
+	'monkoose/matchparen.nvim',
 
 	-- UI
-	use 'rebelot/heirline.nvim'
-	use 'lukas-reineke/indent-blankline.nvim'
-	use {
+	'rebelot/heirline.nvim',
+	'lukas-reineke/indent-blankline.nvim',
+	{
 		'folke/noice.nvim',
 		requires = {
 			'MunifTanjim/nui.nvim',
 			'rcarriga/nvim-notify',
 		}
-	}
+	},
 
 	-- Treesitter
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-	use { 'nvim-treesitter/playground', opt = true }
-	use 'nvim-treesitter/nvim-treesitter-context'
-	use 'p00f/nvim-ts-rainbow'
+	{ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+	{ 'nvim-treesitter/playground', opt = true },
+	-- 'nvim-treesitter/nvim-treesitter-context',
+	'p00f/nvim-ts-rainbow',
 
 	-- Completion
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-path'
-	use 'lukas-reineke/cmp-rg'
-	use 'saadparwaiz1/cmp_luasnip'
+	'hrsh7th/nvim-cmp',
+	'hrsh7th/cmp-buffer',
+	'hrsh7th/cmp-cmdline',
+	'hrsh7th/cmp-nvim-lsp',
+	'hrsh7th/cmp-path',
+	'lukas-reineke/cmp-rg',
+	'saadparwaiz1/cmp_luasnip',
 
-	use 'L3MON4D3/LuaSnip'
-	use 'windwp/nvim-autopairs'
-	use { 'github/copilot.vim', opt = true }
+	'L3MON4D3/LuaSnip',
+	'windwp/nvim-autopairs',
+	-- 'zbirenbaum/copilot.lua',
+	-- 'zbirenbaum/copilot-cmp',
 
 	-- LSP
-	use 'williamboman/mason.nvim'
-	use 'williamboman/mason-lspconfig.nvim'
-	use 'neovim/nvim-lspconfig'
-	use { 'jose-elias-alvarez/null-ls.nvim', requires = 'nvim-lua/plenary.nvim' }
+	'williamboman/mason.nvim',
+	'williamboman/mason-lspconfig.nvim',
+	'neovim/nvim-lspconfig',
+	{ 'jose-elias-alvarez/null-ls.nvim', requires = 'nvim-lua/plenary.nvim' },
 
-	use 'stevearc/aerial.nvim'
-	use 'kosayoda/nvim-lightbulb'
-	use 'folke/neodev.nvim'
+	'glepnir/lspsaga.nvim',
+	'stevearc/aerial.nvim',
+	'folke/neodev.nvim',
 
-	use { 'folke/trouble.nvim', requires = 'nvim-tree/nvim-web-devicons' }
-	-- use 'theHamsta/nvim-semantic-tokens'
+	{ 'folke/trouble.nvim', requires = 'nvim-tree/nvim-web-devicons' },
+	-- 'theHamsta/nvim-semantic-tokens',
 
 	-- Telescope
-	use {
+	{
 		'nvim-telescope/telescope.nvim',
-		branch = '0.1.x',
 		requires = {
 			'nvim-lua/plenary.nvim',
 			'nvim-tree/nvim-web-devicons'
 		}
-	}
+	},
 
-	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-	use { 'nvim-telescope/telescope-frecency.nvim', requires = 'kkharji/sqlite.lua' }
-	use 'nvim-telescope/telescope-file-browser.nvim'
-	use 'nvim-telescope/telescope-bibtex.nvim'
-	use 'nvim-telescope/telescope-ui-select.nvim'
+	{ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+	{ 'nvim-telescope/telescope-frecency.nvim', requires = 'kkharji/sqlite.lua' },
+	'nvim-telescope/telescope-file-browser.nvim',
+	'nvim-telescope/telescope-bibtex.nvim',
+	'nvim-telescope/telescope-ui-select.nvim',
 
 	-- File Exploer
-	use {
+	{
 		'nvim-neo-tree/neo-tree.nvim',
 		branch = 'v2.x',
 		requires = {
@@ -92,24 +92,24 @@ require('packer').startup(function(use)
 			'nvim-tree/nvim-web-devicons',
 			'MunifTanjim/nui.nvim'
 		}
-	}
+	},
 
 	-- Utils
-	use 'numtostr/FTerm.nvim'
-	use 'numToStr/Comment.nvim'
-	use 'lewis6991/gitsigns.nvim'
-	use 'ggandor/leap.nvim'
-	use 'kylechui/nvim-surround'
+	'numtostr/FTerm.nvim',
+	'numToStr/Comment.nvim',
+	'lewis6991/gitsigns.nvim',
+	'ggandor/leap.nvim',
+	'kylechui/nvim-surround',
 
 	-- Tex
-	use 'lervag/vimtex'
-	use { 'f3fora/nvim-texlabconfig', run = 'go build' }
+	{ 'lervag/vimtex' },
+	{ 'f3fora/nvim-texlabconfig', run = 'go build' },
 
 	-- Norg
-	-- use {
+	-- {
 	-- 	'nvim-neorg/neorg',
 	-- 	requires = 'nvim-neorg/neorg-telescope'
-	-- }
+	-- },
+}
 
-	-- use 'dstein64/vim-startuptime'
-end)
+require('packer').startup({ plugins })
