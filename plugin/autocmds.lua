73d67dfada9e06656cf09e2e40479396422f7cd0
mathjiajia <jiajia2u@vip.qq.com>
2022-11-11 21:21:27 +0800

    refactor(autocmds): remove `lcd`; add `q`

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -7,11 +7,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 	desc     = 'Highlight the yanked text',
 })
 
-vim.api.nvim_create_augroup('init_nvim', { clear = true })
-vim.api.nvim_create_autocmd('BufEnter', {
-	command = 'silent! lcd %:p:h',
-	group   = 'init_nvim',
-	desc    = 'change the working directory',
+vim.api.nvim_create_autocmd('FileType', {
+	callback = function()
+		vim.keymap.set('n', 'q', '<Cmd>close<CR>', { buffer = true, silent = true })
+		vim.keymap.set('n', '<Esc>', '<Cmd>close<CR>', { buffer = true, silent = true })
+	end,
+	pattern = { 'help', 'man', 'qf', 'tsplaygound' }
 })
 
 -- vim.api.nvim_create_autocmd('LSPAttach', {
