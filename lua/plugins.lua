1f6f60ee9df8244d6d5106b25489011a773b3f80
mathjiajia <jiajia2u@vip.qq.com>
2022-11-16 09:48:34 +0800

    style(misc): formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,4 @@
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 	vim.fn.system({
 		'git',
