52169e9ecbb16fcc218204ad15571739d353097e
Jia Jia <jiajia2u@vip.qq.com>
2022-11-17 22:21:36 +0800

    fix(heirline): `redraw` error

--- after/plugin/heirline.lua
+++ after/plugin/heirline.lua
@@ -436,17 +436,16 @@ local Ruler = {
 	provider = '%7(%l/%3L%):%2c %P',
 }
 
-
 local ScrollBar = {
 	static = {
-		sbar = { '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█' }
-		-- sbar = { '🭶', '🭷', '🭸', '🭹', '🭺', '🭻' }
+		sbar = { '▁▁', '▂▂', '▃▃', '▄▄', '▅▅', '▆▆', '▇▇', '██' }
+		-- sbar = { '🭶🭶', '🭷🭷', '🭸🭸', '🭹🭹', '🭺🭺', '🭻🭻' }
 	},
 	provider = function(self)
 		local curr_line = vim.api.nvim_win_get_cursor(0)[1]
 		local lines = vim.api.nvim_buf_line_count(0)
 		local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
-		return string.rep(self.sbar[i], 2)
+		return self.sbar[i]
 	end,
 	hl = { fg = 'blue', bg = 'bright_bg' },
 }
