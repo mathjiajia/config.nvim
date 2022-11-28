9a5e9ad8d4fa6946776da0e9d667e4b238828329
Jia Jia <jiajia2u@vip.qq.com>
2022-11-28 16:05:41 +0800

    style(misc): formatting

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -1,3 +1,8 @@
-vim.keymap.set('n', 'q', function()
-	vim.api.nvim_win_close(0, false)
-end, { buffer = true, silent = true })
+vim.keymap.set(
+	'n',
+	'q',
+	function()
+		vim.api.nvim_win_close(0, false)
+	end,
+	{ buffer = true, silent = true }
+)
