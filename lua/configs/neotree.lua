c9d04987cf71b2ca73b084bc2e3a83e72cb9cf45
Jia Jia <jiajia2u@vip.qq.com>
2022-11-28 09:05:47 +0800

    feat(neotree): float with top right

--- lua/configs/neotree.lua
+++ lua/configs/neotree.lua
@@ -1,7 +1,22 @@
 vim.g.neo_tree_remove_legacy_commands = 1
 
 require('neo-tree').setup({
-	window = { width = 30 },
+	window = {
+		position = 'float',
+		popup = {
+			position = {
+				row = 0,
+				col = '100%',
+			},
+			size = {
+				width = 37,
+				height = '50%'
+			},
+			border = {
+				style = 'rounded',
+			},
+		},
+	},
 	filesystem = {
 		follow_current_file = true,
 		hijack_netrw_behavior = 'open_current',
