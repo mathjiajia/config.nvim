783f3dcc06858c9f301b0d73384ac7ac4d8232ee
Jia Jia <jiajia2u@vip.qq.com>
2022-11-30 20:59:54 +0800

    feat(lsp): use `tectonic` for `texlab`

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -104,11 +104,29 @@ require('neodev').setup()
 require('mason').setup()
 require('mason-lspconfig').setup()
 
+local build_executable = 'tectonic'
+local build_args = {
+	'-X',
+	'compile',
+	'%f',
+	'--synctex',
+	'--keep-logs',
+	'--keep-intermediates',
+}
+-- local build_executable = 'latexmk'
+-- local build_args = {
+-- 	'-xelatex',
+-- 	-- '-verbose',
+-- 	'-synctex=1',
+-- 	'-interaction=nonstopmode',
+-- 	'%f',
+-- }
+
 local binary_path = fn.stdpath('data') .. '/site/pack/packer/start/nvim-texlabconfig/nvim-texlabconfig'
 local cache_root = fn.stdpath('cache')
 
-local executable = '/Applications/sioyek.app/Contents/MacOS/sioyek'
-local args = {
+local forward_executable = '/Applications/sioyek.app/Contents/MacOS/sioyek'
+local forward_args = {
 	'--inverse-search',
 	binary_path .. ' -file %1 -line %2 -cache_root ' .. cache_root,
 	'--reuse-instance',
@@ -151,19 +169,14 @@ require('mason-lspconfig').setup_handlers {
 			settings = {
 				texlab = {
 					build = {
-						args = {
-							'-xelatex',
-							-- '-verbose',
-							'-synctex=1',
-							'-interaction=nonstopmode',
-							'%f',
-						},
+						executable = build_executable,
+						args = build_args,
 						-- forwardSearchAfter = true,
 						-- onSave = false,
 					},
 					forwardSearch = {
-						executable = executable,
-						args = args
+						executable = forward_executable,
+						args = forward_args
 					},
 				},
 			},
