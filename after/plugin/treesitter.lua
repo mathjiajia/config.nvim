cd02cc3ee87bd484d9b75e8dff9e5306920dfcf8
Jia Jia <jiajia2u@vip.qq.com>
2022-12-01 22:07:13 +0800

    feat(treesitter): enable `text-objects`

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -32,10 +32,66 @@ require('nvim-treesitter.configs').setup({
 		additional_vim_regex_highlighting = { 'latex' }
 	},
 	incremental_selection = { enable = true },
-	textobjects = { enable = true },
 	indent = { enable = true },
 	-- pairs = { enable = true },
 	rainbow = { enable = true },
 	-- playground = { enable = true },
 	-- query_linter = { enable = true },
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				['ab'] = '@block.outer',
+				['ib'] = '@block.inner',
+				['ac'] = '@class.outer',
+				['ic'] = '@class.inner',
+				['af'] = '@function.outer',
+				['if'] = '@function.inner',
+				['as'] = '@statement.outer',
+			}
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				['<leader>a'] = '@parameter.inner',
+			},
+			swap_previous = {
+				['<leader>A'] = '@parameter.inner',
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				[']m'] = '@function.outer',
+				[']]'] = '@class.outer',
+				[']}'] = '@block.outer',
+			},
+			goto_next_end = {
+				[']M'] = '@function.outer',
+				[']['] = '@class.outer',
+				[']{'] = '@block.outer',
+			},
+			goto_previous_start = {
+				['[m'] = '@function.outer',
+				['[['] = '@class.outer',
+				['[{'] = '@block.outer',
+			},
+			goto_previous_end = {
+				['[M'] = '@function.outer',
+				['[]'] = '@class.outer',
+				['[}'] = '@block.outer',
+			},
+		},
+		lsp_interop = {
+			enable = true,
+			border = 'none',
+			peek_definition_code = {
+				['<leader>df'] = '@function.outer',
+				['<leader>dc'] = '@class.outer',
+				['<leader>db'] = '@block.outer',
+			}
+		}
+	},
 })
