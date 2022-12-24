47091846a74bf5ae7f78760cf5cc9668cc2cf9ae
Jia Jia <jiajia2u@vip.qq.com>
2022-12-24 22:06:27 +0800

    feat(plugin): try `knap`

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -5,6 +5,9 @@ vim.wo.spell = true
 
 vim.keymap.set('i', '<M-l>', '<C-g>u<Esc>[s1z=`]a<C-g>u', { buffer = true, desc = 'Fix Last Miss-Spelling' })
 
+vim.keymap.set('n', '<M-b>', vim.cmd.TexlabBuild, { desc = 'Build LaTeX' })
+vim.keymap.set('n', '<M-f>', vim.cmd.TexlabForward, { desc = 'Forward Search' })
+
 require('nvim-surround').buffer_setup({
 	surrounds = {
 		['"'] = {
