b35b8f27c5a4e7af558ed14cb89dbe654bced6c9
Jia Jia <jiajia2u@vip.qq.com>
2022-12-28 19:45:17 +0800

    refactor(plugins): simplify config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,7 +1,6 @@
 local M = {
 	'hrsh7th/nvim-cmp',
 	dependencies = {
-		'onsails/lspkind.nvim',
 		'hrsh7th/cmp-buffer',
 		'hrsh7th/cmp-cmdline',
 		'hrsh7th/cmp-nvim-lsp',
@@ -14,95 +13,35 @@ local M = {
 }
 
 function M.config()
-	-- local cmp_kinds = {
-	-- 	Text          = '',
-	-- 	Method        = '',
-	-- 	Function      = '',
-	-- 	Constructor   = '',
-	-- 	Field         = '',
-	-- 	Variable      = '',
-	-- 	Class         = '',
-	-- 	Interface     = '',
-	-- 	Module        = '',
-	-- 	Property      = '',
-	-- 	Unit          = '',
-	-- 	Value         = '',
-	-- 	Enum          = '',
-	-- 	Keyword       = '',
-	-- 	Snippet       = '',
-	-- 	Color         = '',
-	-- 	File          = '',
-	-- 	Reference     = '',
-	-- 	Folder        = '',
-	-- 	EnumMember    = '',
-	-- 	Constant      = '',
-	-- 	Struct        = '',
-	-- 	Event         = '',
-	-- 	Operator      = '',
-	-- 	TypeParameter = '',
-	-- }
-
-	local icons = {
-		kind = {
-			Class         = '',
-			Color         = '',
-			Constant      = '',
-			Constructor   = '',
-			Enum          = '',
-			EnumMember    = '',
-			Event         = '',
-			Field         = '',
-			File          = '',
-			Folder        = '',
-			Function      = '',
-			Interface     = '',
-			Keyword       = '',
-			Method        = '',
-			Module        = '',
-			Namespace     = '',
-			Number        = '',
-			Operator      = '',
-			Package       = '',
-			Property      = '',
-			Reference     = '',
-			Snippet       = '',
-			Struct        = '',
-			Text          = '',
-			TypeParameter = '',
-			Unit          = '',
-			Value         = '',
-			Variable      = '',
-			-- ccls-specific icons.
-			TypeAlias     = '',
-			Parameter     = '',
-			StaticMethod  = '',
-			Macro         = '',
-		},
-
-		type = {
-			Array   = '',
-			Boolean = '',
-			Null    = 'ﳠ',
-			Number  = '',
-			Object  = '',
-			String  = '',
-		},
-
-		cmp = {
-			Copilot     = '',
-			Copilot_alt = '',
-			nvim_lsp    = '',
-			nvim_lua    = '',
-			path        = '',
-			buffer      = ' ',
-			spell       = '暈',
-			luasnip     = '',
-			treesitter  = '',
-		}
+	local cmp_kinds = {
+		Text          = '',
+		Method        = '',
+		Function      = '',
+		Constructor   = '',
+		Field         = '',
+		Variable      = '',
+		Class         = '',
+		Interface     = '',
+		Module        = '',
+		Property      = '',
+		Unit          = '',
+		Value         = '',
+		Enum          = '',
+		Keyword       = '',
+		Snippet       = '',
+		Color         = '',
+		File          = '',
+		Reference     = '',
+		Folder        = '',
+		EnumMember    = '',
+		Constant      = '',
+		Struct        = '',
+		Event         = '',
+		Operator      = '',
+		TypeParameter = ''
 	}
 
 	local cmp = require('cmp')
-	local lspkind = require('lspkind')
 	local border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' }
 
 	cmp.setup({
@@ -120,16 +59,18 @@ function M.config()
 		formatting = {
 			fields = { 'kind', 'abbr', 'menu' },
 			format = function(entry, vim_item)
-				local kind = lspkind.cmp_format({
-					mode = 'symbol_text',
-					maxwidth = 50,
-					symbol_map = vim.tbl_deep_extend('force', icons.kind, icons.type, icons.cmp),
-				})(entry, vim_item)
-				local strings = vim.split(kind.kind, '%s', { trimempty = true })
-				kind.kind = strings[1]
-				kind.menu = ' (' .. strings[2] .. ')'
-				return kind
-			end,
+				vim_item.kind = cmp_kinds[vim_item.kind]
+				vim_item.menu = ({
+					buffer   = '[Buf]',
+					cmdline  = '[Cmd]',
+					luasnip  = '[Snip]',
+					nvim_lsp = '[LSP]',
+					-- neorg    = '[Norg]',
+					path     = '[Path]',
+					rg       = '[RG]',
+				})[entry.source.name]
+				return vim_item
+			end
 		},
 		matching = { disallow_prefix_unmatching = true },
 		sources = {
@@ -143,13 +84,13 @@ function M.config()
 		},
 		window = {
 			completion = {
-				border = border,
+				border = 'rounded',
 				max_width = 80,
 				max_height = 20,
 				col_offset = -3,
 			},
 			documentation = {
-				border = border,
+				border = 'rounded',
 			},
 		}
 	})
--- lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		'dstein64/vim-startuptime',
-		config = function()
-			vim.g.startuptime_tries = 10
-		end,
-		cmd = 'StartupTime',
-	},
-}
