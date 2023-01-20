222043caec0c576e28310c220aa89c8c5f4dd32f
Jia Jia <jiajia2u@vip.qq.com>
2023-01-20 10:21:09 +0800

    feat(copilot): config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -90,6 +90,7 @@ return {
 							nvim_lsp = "[LSP]",
 							luasnip = "[Snip]",
 							neorg = "[Norg]",
+							-- copilot = "[GHC]",
 							path = "[Path]",
 							rg = "[RG]",
 						})[entry.source.name]
@@ -116,52 +117,13 @@ return {
 	},
 
 	-- github copilot
-	{
-		"zbirenbaum/nvim-copilot",
-		enabled = false,
-		dependencies = { "zbirenbaum/copilot_cmp", config = true },
-		opts = {
-			panel = {
-				enabled = true,
-				auto_refresh = false,
-				keymap = {
-					jump_prev = "[[",
-					jump_next = "]]",
-					accept = "<CR>",
-					refresh = "gr",
-					open = "<M-CR>",
-				},
-			},
-			suggestion = {
-				enabled = true,
-				auto_trigger = false,
-				debounce = 75,
-				keymap = {
-					accept = "<M-l>",
-					accept_word = false,
-					accept_line = false,
-					next = "<M-]>",
-					prev = "<M-[>",
-					dismiss = "<C-]>",
-				},
-			},
-			filetypes = {
-				yaml = false,
-				markdown = false,
-				help = false,
-				gitcommit = false,
-				gitrebase = false,
-				hgcommit = false,
-				svn = false,
-				cvs = false,
-				["."] = false,
-			},
-			copilot_node_command = "node",
-			server_opts_overrides = {},
-		},
-		event = "InsertEnter",
-		cmd = "Copilot",
-	},
+	-- {
+	-- 	"zbirenbaum/copilot.lua",
+	-- 	config = true,
+	-- 	dependencies = { "zbirenbaum/copilot-cmp", config = true },
+	-- 	event = "InsertEnter",
+	-- 	cmd = "Copilot",
+	-- },
 
 	-- auto pairs
 	{
