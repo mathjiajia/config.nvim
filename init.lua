9990a45cfa01de803ac91b18b15060a855f492fd
Jia Jia <mathjiajia@outlook.com>
2024-05-16 20:51:50 +0800

    chore: move files

--- init.lua
+++ init.lua
@@ -7,4 +7,41 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 
 require("config.options")
-require("config.lazyinit")
+require("config.keymaps")
+require("config.autocmds")
+
+---------- LAZYINIT ----------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+	vim.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins", {
+	dev = { patterns = { "mathjiajia" } },
+	ui = { border = "rounded" },
+	change_detection = { notify = false },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"rplugin",
+				"shada",
+				"spellfile",
+				"tarPlugin",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
